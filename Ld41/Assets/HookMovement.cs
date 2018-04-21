12b782af35278a7bd2744a7ead3289ab3621ba86
ajpowell021 <ajpowell021@gmail.com>
2018-04-20 23:29:55 -0400

    Basic art and movement.

---
+++ Ld41/Assets/HookMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HookMovement : MonoBehaviour {
+
+	// Public Functions
+
+	public void moveHook(Vector3 direction) {
+		RaycastHit hit;
+		if (!Physics.Raycast(transform.position, direction, out hit, 1)) {
+			transform.position = Vector3.Lerp(transform.position, transform.position + direction, 1);
+		}
+	}
+}
---
+++ Ld41/Assets/InputManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour {
+
+	// Private State
+
+	private HookMovement hookMovement;
+
+	// Init
+
+	private void Start() {
+		hookMovement = GameObject.FindGameObjectWithTag("Hook").GetComponent<HookMovement>();
+	}
+
+	// Update
+	private void Update () {
+		checkHookmovement();
+	}
+
+	// Hook Movement
+
+	private void checkHookmovement() {
+		if (Input.GetKeyDown("s")) {
+			hookMovement.moveHook(Vector3.down);
+		}
+		else if (Input.GetKeyDown("a")) {
+			hookMovement.moveHook(Vector3.left);
+		}
+		else if (Input.GetKeyDown("d")) {
+			hookMovement.moveHook(Vector3.right);
+		}
+		else if (Input.GetKeyDown("w")) {
+			hookMovement.moveHook(Vector3.up);
+		}
+	}
+}
